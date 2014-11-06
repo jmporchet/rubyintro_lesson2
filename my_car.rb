1781c3f12e7f89cb99bfcb05f700e5a8b7a0c926
Jean-Marie Porchet <jm@jmporchet.ch>
2014-11-06 22:44:27 +0100

    =continuation of the exercises

--- my_car.rb
+++ my_car.rb
@@ -1,12 +1,23 @@
-class MyCar
+module Loadable
+  def reverse
+    puts "beep beep beep"
+  end
+end
+
+class Vehicle
+
   attr_accessor :color
   attr_reader :year
+  @@number_of_vehicles = 0
+
+  VEHICLE = ""
 
   def initialize(year, color, model)
     @year = year
     @color = color
     @model = model
     @speed = 0
+    @@number_of_vehicles += 1
   end
 
   def accelerate
@@ -29,6 +40,38 @@ class MyCar
     self.color = color
   end
 
+
+  def self.calculate_mileage liters, kilometers
+    "#{(liters.to_f/kilometers.to_f)*100} l/100km"
+  end
+
+  def self.number_of_vehicles
+    puts "This program has created #{@@number_of_vehicles} vehicles"
+  end
+
+  def age
+    puts "The vehicle is #{calculate_age} years old"
+  end
+
+  private
+
+  def calculate_age
+    Time.now.year - self.year
+  end
+end
+
+class MyCar < Vehicle
+
+  VEHICLE = "Car"
+
+  def to_s
+    "I am a #{self.color} #{@model} of year #{self.year}, and I'm driving at #{@speed}"
+  end
+end
+
+class MyTruck < Vehicle
+  include Loadable
+  VEHICLE = 'Truck'
 end
 
 golf = MyCar.new(2013, 'black', 'VW Golf')
@@ -48,3 +91,15 @@ puts golf.year
 puts golf.color
 golf.spray_paint 'yellow'
 puts golf.color
+
+puts MyCar.calculate_mileage 45, 550
+
+polo = MyCar.new 2011, 'grey', 'VW Polo'
+
+scania = MyTruck.new 2009, 'silver', 'scania truck'
+Vehicle.number_of_vehicles
+scania.reverse
+
+puts MyTruck.ancestors
+
+scania.age
