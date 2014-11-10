23ed9cf3dee6679b86cece2cadb13d5e994a0ac3
Jean-Marie Porchet <jm@jmporchet.ch>
2014-11-10 22:09:12 +0100

    =Extract nouns and outline basic functionality

---
+++ oo_blackjack.rb
@@ -0,0 +1,5 @@
+Game creates Dealer and Player, evaluates hands, allows to continue
+Dealer creates and shuffles deck, hits cards from the deck and stays
+Player hits cards from the deck and stays
+Deck holds cards
+Card
