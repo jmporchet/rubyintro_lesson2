9333ed5d491decd05f4cd6b5bc36e1c3467901f3
Jean-Marie Porchet <jm@jmporchet.ch>
2014-11-12 17:48:55 +0100

    =generate a specified number of decks at the beginning of the game

--- oo_blackjack.rb
+++ oo_blackjack.rb
@@ -11,13 +11,15 @@ class Game
     
     puts "What's your name?"
     player_one = Human.new gets.chomp
+
+    puts "Number of decks (default: 3)?"
+    nr_of_decks = gets.chomp.to_i
     
     decks = []
-    3.times do |t| 
-      decks << Deck.new
-    end
-    binding.pry
+    decks = Deck.new(nr_of_decks)
     #now merge all decks into one only. flatten! will not work.
+    #flat_deck = decks[0].merge(decks[1])
+    binding.pry
   end
 
   def run
@@ -45,13 +47,15 @@ end
 class Deck
   attr_accessor :suits, :values, :deck
 
-  def initialize
+  def initialize nr_of_decks = 3
     self.deck = []
     suits = ['D', 'H', 'C','S']
     values = [1,2,3,4,5,6,7,8,9,10,11,12,13]
-    suits.each do |suit|
-      values.each do |value|
-        self.deck << Card.new(suit, value)
+    nr_of_decks.times do
+      suits.each do |suit|
+        values.each do |value|
+          self.deck << Card.new(suit, value)
+        end
       end
     end
 
