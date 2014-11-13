6dcbcb4fc0f99f2aafbde0b201dd5dbfb2fa3699
Jean-Marie Porchet <jm@jmporchet.ch>
2014-11-13 09:47:02 +0100

    =checks if busted

--- oo_blackjack.rb
+++ oo_blackjack.rb
@@ -8,6 +8,8 @@ require 'pry'
 class Game
   attr_accessor :players, :decks
 
+  THRESHOLD = 21
+
   def initialize
     self.players = {}
     puts "What's your name?"
@@ -26,10 +28,29 @@ class Game
     #flat_deck = decks[0].merge(decks[1])
   end
 
+  def who_won
+
+  end
+
   def play
     players.each do |k,v|
       v.hit decks
       v.hit decks
+    end
+
+    players.each do |k,v|
+      puts "#{v.name} has " + v.calculate_hand.to_s
+    end
+
+    while !players[:player_one].has_busted
+      puts "1) hit or 2) stay?"
+      answer = gets.chomp.to_i
+      if answer == 1
+        players[:player_one].hit decks
+        puts "Your score: " + players[:player_one].calculate_hand.to_s
+      else 
+        break
+      end
     end
 
     while players[:dealer].calculate_hand < 17
@@ -37,23 +58,23 @@ class Game
       puts "dealer hits one more time! " + players[:dealer].calculate_hand.to_s
     end
 
-    players.each do |k,v|
-      puts "#{v.name} has " + v.calculate_hand.to_s
-    end
-
-    if players[:dealer].calculate_hand > 21
+    if players[:dealer].has_busted
       puts "dealer busted!"
       exit
     end
+
+    puts who_won
 
   end
 end
 
 class Player
-  attr_accessor :decks, :hand
+  attr_accessor :decks, :hand, :has_busted, :score
 
   def initialize
     self.hand = []
+    self.has_busted = false
+    self.score = 0
   end
 
   def hit decks
@@ -64,13 +85,16 @@ class Player
     amount = 0
     self.hand = hand.sort_by { |k,v| v }
     hand.flatten.each do |card|
+      #if the Ace value makes the blayer bust, transform it to 1 instead
       if (card.value == 11) && ((amount + card.value) > 21)
         card.value = 1
-        puts "Ace transformed to 1"
       end
       amount += card.value
     end
-    amount
+    if amount > 21
+      self.has_busted = true
+    end
+    self.score = amount
   end
 end
 
